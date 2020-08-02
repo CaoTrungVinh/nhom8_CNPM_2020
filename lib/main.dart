0e6e076eee058b0a4ff7ed743d4e948e35edf280
Mỹ Duyên <myduyenn218@gmail.com>
2020-08-02 11:05:15 +0700

    edit file up store

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
