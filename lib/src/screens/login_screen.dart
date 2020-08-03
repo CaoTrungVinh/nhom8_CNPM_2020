fb747b5fccfd9c6d1f9e60dbbab3c34c54d06879
Mỹ Duyên <myduyenn218@gmail.com>
2020-08-03 15:07:42 +0700

    update config signin

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-FirebaseAuth _auth = FirebaseAuth.instance;
-FirebaseUser _user;
-GoogleSignIn _googleSignIn = new GoogleSignIn();
+GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['profile', 'email']);
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -14,115 +10,76 @@ class LoginScreen extends StatefulWidget {
   }
 }
 
-//hàm build
 class _loginScreen extends State<LoginScreen> {
+  GoogleSignInAccount _currentUser;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
+      setState(() {
+        _currentUser = account;
+      });
+    });
+    _googleSignIn.signInSilently();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image(
-                  image: AssetImage("assets/images/logo_chat.png"),
-                  height: 150.0),
-              SizedBox(height: 50),
-              _signInButton(),
-            ],
-          ),
-        ),
+      appBar: AppBar(
+        title: Text('Google Sign in Demo'),
       ),
+      body: Center(child: _buildBody()),
     );
   }
-  @override
-  void initState() {
-    super.initState();
-    isSignIn = false;
-  }
-  Widget _signInButton() {
-    return isSignIn
-        ? OutlineButton(
-            splashColor: Colors.grey,
-            onPressed: () {
-              googleSignOut();
-            },
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-            highlightElevation: 0,
-            borderSide: BorderSide(color: Colors.grey),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(_user.photoUrl),
-                  ),
-                ],
-              ),
+
+  Widget _buildBody() {
+    if (_currentUser != null) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          ListTile(
+            leading: GoogleUserCircleAvatar(
+              identity: _currentUser,
             ),
+            title: Text(_currentUser.displayName ?? ''),
+            subtitle: Text(_currentUser.email ?? ''),
+          ),
+          RaisedButton(
+            onPressed: _handleSignOut,
+            child: Text('SIGN OUT'),
           )
-        : OutlineButton(
-            splashColor: Colors.grey,
-            onPressed: () {
-              handleSignIn();
-            },
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-            highlightElevation: 0,
-            borderSide: BorderSide(color: Colors.grey),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Image(
-                      image: AssetImage("assets/images/google_logo.png"),
-                      height: 25.0),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Text(
-                      'Sign in with Google',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          );
+        ],
+      );
+    } else {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Text('You are not signed in..'),
+          RaisedButton(
+            onPressed: _handleSignIn,
+            child: Text('SIGN IN'),
+          )
+        ],
+      );
+    }
   }
 
-  bool isSignIn = false;
-
-  Future<void> handleSignIn() async {
-    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
-    GoogleSignInAuthentication googleSignInAuthentication =
-        await googleSignInAccount.authentication;
-    AuthCredential credential = GoogleAuthProvider.getCredential(
-        idToken: googleSignInAuthentication.idToken,
-        accessToken: googleSignInAuthentication.accessToken);
-    AuthResult result = (await _auth.signInWithCredential(credential));
-    _user = result.user;
-    setState(() {
-      isSignIn = true;
-    });
+  Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
   }
 
-  Future<void> googleSignOut() async {
-    await _auth.signOut().then((value) {
-      _googleSignIn.signOut();
-      setState(() {
-        isSignIn = false;
-      });
-    });
+  Future<void> _handleSignOut() async {
+    _googleSignIn.disconnect();
   }
 }
